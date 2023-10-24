a7cd3cb072e36bdeb501bec2c1a48b5b3066e869
SoniaScard <83462318+SoniaScard@users.noreply.github.com>
2023-10-24 15:37:52 +0200

    Add files via upload

---
+++ multi-node/launch_cluster.sh
@@ -0,0 +1,22 @@
+n_workers=$1
+
+if [ -z "$n_workers" ];then
+    n_workers=1
+fi
+
+echo "Deploying 1 master and ${n_workers} workers..."
+
+# Start MASTER
+docker run -it -d --name ophidia-multinode-master -v /home/container-test/multi-node/data:/usr/local/ophidia/data:Z -p 11732:11732 -e MEMORY=4096 -e DEBUG=1 ophidiabigdata/ophidia-multinode-master:latest || :
+
+while [ -z "$master_ip" ];do
+    master_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ophidia-multinode-master)
+done
+echo $master_ip
+
+# Start WORKERS
+    for i in $(seq 1 $n_workers)
+    do
+        sleep 10
+        docker run -it -d --name ophidia-multinode-worker_$i -v /home/container-test/multi-node/data:/usr/local/ophidia/data -e MEMORY=4096 -e OPHDB_HOST=$master_ip ophidiabigdata/ophidia-multinode-worker:latest
+    done
